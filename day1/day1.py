233c7d5aae71216b7d8b2982859f31c99694a732
My Name <esthergoldman@gmail.com>
2021-04-14 16:15:32 +0300

    first update

---
+++ day1/day1.py
@@ -0,0 +1,34 @@
+# print("day 1 - Python print Function\nThe function is declared like this\nprint('whay to print')")
+
+
+# print("hello\nhello\nhello")
+
+#input() will get user input in console then print() will print "hello" and the user input
+#print('hello ' + input('what is your name\n') + '!')
+
+
+# print(len(input("whats your name\n")))
+
+
+# Instructions
+
+#Write a program that switches the values stored in the variables a and b. 
+
+#**Warning.** Do not change the code on lines 1-4 and 12-18. Your program should work for different inputs. e.g. any value of a and b.
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+a1= a
+b2= b
+a=a1
+b=b2
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
